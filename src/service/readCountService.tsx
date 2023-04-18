c1d6aae870860007e9c85b42411c65f448c0e434
songchez <tama4840@gmail.com>
2023-04-18 11:48:02 +0900

    db연결

--- src/service/readCountService.tsx
+++ src/service/readCountService.tsx
@@ -1,12 +1,23 @@
 import prisma from "@/lib/prisma";
 
 //TODO: 1.유저에 카운트
-export async function UserCount({ userEmail }: { userEmail: string }) {
+export async function UserCount({
+  userEmail,
+  incrVerse,
+  incrChapter,
+}: {
+  userEmail: string;
+  incrVerse: number;
+  incrChapter: number;
+}) {
   const user = await prisma.user.update({
     where: { email: userEmail },
     data: {
       countVerse: {
-        increment: 1,
+        increment: incrVerse,
+      },
+      countChapter: {
+        increment: incrChapter,
       },
     },
   });
